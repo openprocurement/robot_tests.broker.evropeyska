7a6960270cdf4a647756cfd66f62c076542deff2
mxuser <mxuser@ya.ru>
2018-10-16 15:24:30 +0300

    bugfix

--- evropeyska_service.py
+++ evropeyska_service.py
@@ -7,10 +7,6 @@ import os
 from datetime import datetime
 from robot.libraries.BuiltIn import BuiltIn
 
-def get_webdriver():
-   se2lib = BuiltIn().get_library_instance('Selenium2Library')
-   return se2lib._current_browser()
-
 def download_file(url, file_name, output_dir):
    urllib.urlretrieve(url, ('{}/{}'.format(output_dir, file_name)))
 
